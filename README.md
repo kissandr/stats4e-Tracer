0a92484215f5f8dfda5201ccfdc9a6ba36729359
Kiss Andras <andrew@idokep.hu>
2015-03-19 10:37:07 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # stats4e-Trace
+Web archive for Solar e-Tracer 6415N photovoltaic charge manager
+
+Set crontab for getting live data:
+*/5 * * * * php cron.php
+
+Edit $address variable in cron.php to reflect your charge manager's IP address!
+
--- cron.php
+++ cron.php
@@ -1,7 +1,8 @@
 <?
+$address = "http://213.16.87.105";
 ini_set('error_reporting', E_ALL);
 // 025.9:000.0:000.0:0000:000.0:026.0:000.0:Normal  :No Charge :053:0016.4:0018.6:
-$raw = exec('/usr/bin/curl -s --max-time 10 "http://213.16.87.105/RTMonitor?id="');
+$raw = exec('/usr/bin/curl -s --max-time 10 "'.$address.'/RTMonitor?id="');
 //echo $raw;
 if (preg_match('/(\d\d\d\.\d):(\d\d\d.\d):(\d\d\d.\d):(\d\d\d\d):(\d\d\d.\d):(\d\d\d.\d):(\d\d\d.\d):(\S+)\s*:([^:]+)\s*:(\d\d\d):(\d\d\d\d.\d):(\d\d\d\d.\d):/',$raw,$m)) {
     $mysql=@mysql_pconnect("localhost","solar","w83pm2jhD48T");
