09618fa78629cf2d0b026224e57199ce644b8368
Kiss Andras <andrew@idokep.hu>
2015-03-19 10:54:01 +0100

    minor changes

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?
-$db=   $mysql=@mysql_pconnect("localhost","solar","w83pm2jhD48T");
-    mysql_select_db("solar") or die("Adatbázis hiba!");
-    mysql_query("SET NAMES utf8");
+$mysql=@mysql_pconnect("localhost","solar","mypassword12345");
+mysql_select_db("solar") or die(mysql_error());
+mysql_query("SET NAMES utf8");
 ?>
 
